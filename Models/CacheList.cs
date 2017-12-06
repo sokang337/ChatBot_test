5281853c4e4a0ed91b7bc7518c458e649edfb519
sokang337 <sokang337@gmail.com>
2017-12-06 15:04:22 +0900

    Add files via upload

---
+++ Models/CacheList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SecCsChatBotDemo.Models
+{
+    public class CacheList
+    {
+        public string luisId;
+        public string intentId;
+        public string entitiesIds;
+        public int dlgId;
+        public int dlgOrderNo;
+        public string dlgApiDefine;
+    }
+}
---
+++ Models/LuisList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SecCsChatBotDemo.Models
+{
+    public class LuisList
+    {
+        public int dlgId;
+        public string dlgOrderNo;
+
+        public string dlgIntent;
+        public string dlgEntities;
+    }
+}
